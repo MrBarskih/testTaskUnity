a1dd3f1e26fdf3f5579abb4af2d22b96d137ea42
MrBarskih <59989968+MrBarskih@users.noreply.github.com>
2020-01-29 14:34:08 +0700

    1

---
+++ testTask/Assets/Scripts/Enemies.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemies : MonoBehaviour
+{
+    public float speed;
+    private Waypoints wpoints;
+
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, wpoints.waypoints[waypointIndex].position) < 0.1f) {
+            if (waypointIndex < wpoints.waypoints.Length - 1)
+            {
+                waypointIndex++;
+            }
+            else {
+                Destroy(gameObject);
+                SceneManager.LoadScene("Main", LoadSceneMode.Single);
+            }
+           
+        }
+    }
+}
---
+++ testTask/Assets/Scripts/twPlace.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class twPlace : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            PlaceTower();
+        }
+    }
+    private void PlaceTower() {
+        LevelManager lm = GameObject.FindObjectOfType<LevelManager>();
+        Instantiate(lm.TowerPrefab,transform.position, Quaternion.identity);
+    }
+}
